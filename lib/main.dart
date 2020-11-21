eb8c238af2e0950236988869f5bad6da84b99be8
Muhammad AbdulSalam <abdulsalamch32@gmail.com>
2020-11-21 14:18:49 +0000

    updated gradients

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:service_exchange_multi/utils/Constants.dart';
 
 import 'loginviews/LoginActivity.dart';
+import 'package:flutter/animation.dart';
+
+
 
 void main() {
+
+  SystemChrome.setEnabledSystemUIOverlays([]);
+
   runApp(new MaterialApp(
-    home: new SplashScreen(),
+
+      home: new SplashScreen(),
     routes: <String, WidgetBuilder>{
       '/HomeScreen': (BuildContext context) => new LoginActivity()
     },
@@ -19,11 +28,12 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => new _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
 
 
   startTime() async {
-    var _duration = new Duration(seconds: 2);
+    var _duration = new Duration(seconds: 3);
     return new Timer(_duration, navigationPage);
   }
 
@@ -31,37 +41,147 @@ class _SplashScreenState extends State<SplashScreen> {
     Navigator.of(context).pushReplacementNamed('/HomeScreen');
   }
 
+
+  //Animation
+  Animation<double> backgroundAnimation;
+
+  AnimationController _backgroundController;
+
+  // list of bubble widgets shown on screen
+  final bubbleWidgets = List<Widget>();
+
+  // flag to check if the bubbles are already present or not.
+  bool areBubblesAdded = false;
+
+  Animatable<Color> backgroundDark = TweenSequence<Color>([
+    TweenSequenceItem(
+      weight: 0.5,
+      tween: ColorTween(
+        begin: Colors.blue[800],
+        end: Colors.pink[800],
+      ),
+    ),
+    TweenSequenceItem(
+      weight: 0.5,
+      tween: ColorTween(
+        begin: Colors.pink[800],
+        end: Colors.blue[800],
+      ),
+    ),
+  ]);
+  Animatable<Color> backgroundNormal = TweenSequence<Color>([
+    TweenSequenceItem(
+      weight: 0.5,
+      tween: ColorTween(
+        begin: Colors.blue[500],
+        end: Colors.pink[500],
+      ),
+    ),
+    TweenSequenceItem(
+      weight: 0.5,
+      tween: ColorTween(
+        begin: Colors.pink[500],
+        end: Colors.blue[500],
+      ),
+    ),
+  ]);
+  Animatable<Color> backgroundLight = TweenSequence<Color>([
+    TweenSequenceItem(
+      weight: 0.5,
+      tween: ColorTween(
+        begin: Colors.blue[200],
+        end: Colors.pink[200],
+      ),
+    ),
+    TweenSequenceItem(
+      weight: 0.5,
+      tween: ColorTween(
+        begin: Colors.pink[200],
+        end: Colors.blue[200],
+      ),
+    ),
+  ]);
+
+  AlignmentTween alignmentTop = AlignmentTween(begin: Alignment.topRight,end: Alignment.topLeft);
+  AlignmentTween alignmentBottom = AlignmentTween(begin: Alignment.bottomRight,end: Alignment.bottomLeft);
+
   @override
   void initState() {
     super.initState();
-    startTime();
-  }
+    _backgroundController = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    )..repeat();
 
+    backgroundAnimation = CurvedAnimation(parent: _backgroundController, curve: Curves.easeIn)
+      ..addStatusListener((status){
+        if(status == AnimationStatus.completed){
+          setState(() {
+
+            _backgroundController.forward(from: 0);
+          });
+        }
+        if(status == AnimationStatus.dismissed){
+          setState(() {
+            _backgroundController.forward(from: 0);
+          });
+        }
+      });
+
+    startTime();
+
+  }
   @override
   Widget build(BuildContext context) {
-    double width=MediaQuery.of(context).size.width;
-    double height=MediaQuery.of(context).size.height;
 
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: new BoxDecoration(
-              gradient: new LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Constants.DEFAULT_BLUE, Constants.DEFAULT_ORANGE
-                ],
-              )),
-          child: Align(
-              alignment: Alignment.center,
-              child: Image.asset(
-                "images/logo_main.png",
-                width: 200,
-                height: 200,
-              )),
-        ),
-      ),
+    // Add below to add bubbles intially.
+
+    return AnimatedBuilder(
+      animation: backgroundAnimation,
+      builder: (context, child){
+        return Scaffold(
+
+          body:  Stack(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: alignmentTop.evaluate(backgroundAnimation),
+                    end: alignmentBottom.evaluate(backgroundAnimation),
+                    colors: [
+                      backgroundDark.evaluate(backgroundAnimation),
+                      backgroundNormal.evaluate(backgroundAnimation),
+                      backgroundLight.evaluate(backgroundAnimation),
+
+                    ],
+                  ),
+                ),
+              ),
+
+
+            ]
+                +[Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: new Image.asset("images/logo_main.png", width: 170, height: 170)
+                    ),
+                  ],
+                ),],
+          ),
+
+
+        );
+      },
     );
   }
+  @override
+  void dispose() {
+    super.dispose();
+    _backgroundController.dispose();
+  }
+
 }
+
