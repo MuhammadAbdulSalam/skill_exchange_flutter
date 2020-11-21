bc18281fc6092543d947f2c1d17d725198fababb
Muhammad AbdulSalam <abdulsalamch32@gmail.com>
2020-11-21 23:42:27 +0000

    location autocomplete

--- lib/location/AddressAutoComplete.dart
+++ lib/location/AddressAutoComplete.dart
@@ -1,63 +1,33 @@
-import 'dart:async';
-import 'dart:ffi';
-
-import 'package:google_maps_webservice/places.dart';
-import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
-
 import 'package:service_exchange_multi/utils/Constants.dart';
+import 'package:uuid/uuid.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-const kGoogleApiKey = "AIzaSyANJXhDPNxefcF2P0JXXcz0PxG7ikPBgBA";
-
-// to get places detail (lat/lng)
-GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
-
-final customTheme = ThemeData(
-  primarySwatch: Colors.blue,
-  brightness: Brightness.dark,
-  accentColor: Colors.redAccent,
-  inputDecorationTheme: InputDecorationTheme(
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.all(Radius.circular(4.00)),
-    ),
-    contentPadding: EdgeInsets.symmetric(
-      vertical: 12.50,
-      horizontal: 10.00,
-    ),
-  ),
-);
+import 'AddressSearch.dart';
+import 'PlaceService.dart';
 
 class AddressAutoComplete extends StatefulWidget {
   @override
-  _AddressAutoComplete createState() => _AddressAutoComplete();
+  _AddressAutoCompleteState createState() => _AddressAutoCompleteState();
 }
 
-final homeScaffoldKey = GlobalKey<ScaffoldState>();
-final searchScaffoldKey = GlobalKey<ScaffoldState>();
-var mContext;
+class _AddressAutoCompleteState extends State<AddressAutoComplete> {
+  final _controller = TextEditingController();
+  String _streetNumber = '';
+  String _street = '';
+  String _city = '';
+  String _zipCode = '';
 
-class _AddressAutoComplete extends State<AddressAutoComplete> {
-  var firstStart = true;
-
-  void initState() {
-    super.initState();
-
-    if (firstStart) {
-      WidgetsBinding.instance.addPostFrameCallback((_) => Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => CustomSearchScaffold()),
-          ));
-    }
-    firstStart = false;
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    mContext = context;
-
     return Scaffold(
-      key: homeScaffoldKey,
       appBar: AppBar(
         centerTitle: true,
         title: Text("Register"),
@@ -72,153 +42,56 @@ class _AddressAutoComplete extends State<AddressAutoComplete> {
               ])),
         ),
       ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          CustomSearchScaffold()));
-            },
-            child: Text("Search places"),
-          ),
-          // RaisedButton(
-          //   child: Text("Custom"),
-          //   onPressed: () {
-          //     Navigator.of(context).pushNamed("/search");
-          //   },
-          // ),
-        ],
-      )),
-    );
-  }
-
-  void onError(PlacesAutocompleteResponse response) {
-    homeScaffoldKey.currentState.showSnackBar(
-      SnackBar(content: Text(response.errorMessage)),
-    );
-  }
-
-  Future<void> _handlePressButton() async {
-    // show input autocomplete with selected mode
-    // then get the Prediction selected
-    Prediction p = await PlacesAutocomplete.show(
-      context: context,
-      apiKey: kGoogleApiKey,
-      onError: onError,
-      mode: Mode.fullscreen,
-      language: "en",
-      logo: Text(""),
-    );
-
-    displayPrediction(p, homeScaffoldKey.currentState);
-  }
-}
-
-Future<Null> displayPrediction(Prediction p, ScaffoldState scaffold) async {
-  if (p != null) {
-    // get detail (lat/lng)
-    PlacesDetailsResponse detail = await _places.getDetailsByPlaceId(p.placeId);
-    final lat = detail.result.geometry.location.lat;
-    final lng = detail.result.geometry.location.lng;
-
-    scaffold.showSnackBar(
-      SnackBar(content: Text("${p.description} - $lat/$lng")),
-    );
-
-    Navigator.pop(mContext);
-  }
-}
-
-// custom scaffold that handle search
-// basically your widget need to extends [GooglePlacesAutocompleteWidget]
-// and your state [GooglePlacesAutocompleteState]
-class CustomSearchScaffold extends PlacesAutocompleteWidget {
-  CustomSearchScaffold()
-      : super(
-          apiKey: kGoogleApiKey,
-          sessionToken: Uuid().generateV4(),
-          language: "en",
-          components: [Component(Component.country, "uk")],
-        );
-
-  @override
-  _CustomSearchScaffoldState createState() => _CustomSearchScaffoldState();
-}
-
-class _CustomSearchScaffoldState extends PlacesAutocompleteState {
-  @override
-  Widget build(BuildContext context) {
-    final appBar = AppBar(
-      centerTitle: true,
-      title: AppBarPlacesAutoCompleteTextField(),
-      flexibleSpace: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: <Color>[
-              Constants.DEFAULT_ORANGE,
-              Constants.DEFAULT_BLUE,
-            ])),
+      body: Container(
+        margin: EdgeInsets.only(left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              readOnly: true,
+              onTap: () async {
+                // generate a new token here
+                final sessionToken = Uuid().v4();
+                final Suggestion result = await showSearch(
+                  context: context,
+                  delegate: AddressSearch(sessionToken),
+                );
+                // This will change the text displayed in the TextField
+                if (result != null) {
+                  final placeDetails = await PlaceApiProvider(sessionToken)
+                      .getPlaceDetailFromId(result.placeId);
+                  setState(() {
+                    _controller.text = result.description;
+                    _streetNumber = placeDetails.streetNumber;
+                    _street = placeDetails.street;
+                    _city = placeDetails.city;
+                    _zipCode = placeDetails.zipCode;
+                  });
+                }
+              },
+              decoration: InputDecoration(
+                icon: Container(
+                  width: 10,
+                  height: 10,
+                  child: Icon(
+                    Icons.home,
+                    color: Colors.black,
+                  ),
+                ),
+                hintText: "Enter your shipping address",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 8.0, top: 16.0),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text('Street Number: $_streetNumber'),
+            Text('Street: $_street'),
+            Text('City: $_city'),
+            Text('ZIP Code: $_zipCode'),
+          ],
+        ),
       ),
     );
-
-    final body = PlacesAutocompleteResult(
-      onTap: (p) {
-        displayPrediction(p, searchScaffoldKey.currentState);
-      },
-      logo: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-      ),
-    );
-    return Scaffold(key: searchScaffoldKey, appBar: appBar, body: body);
   }
-
-  @override
-  void onResponseError(PlacesAutocompleteResponse response) {
-    super.onResponseError(response);
-
-    searchScaffoldKey.currentState.showSnackBar(
-      SnackBar(content: Text(response.errorMessage)),
-    );
-  }
-
-  @override
-  void onResponse(PlacesAutocompleteResponse response) {
-    super.onResponse(response);
-
-    if (response != null && response.predictions.isNotEmpty) {
-      searchScaffoldKey.currentState.showSnackBar(
-        SnackBar(content: Text(response.predictions.toString())),
-      );
-    }
-  }
-}
-
-class Uuid {
-  final Random _random = Random();
-
-  String generateV4() {
-    // Generate xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx / 8-4-4-4-12.
-    final int special = 8 + _random.nextInt(4);
-
-    return '${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}-'
-        '${_bitsDigits(16, 4)}-'
-        '4${_bitsDigits(12, 3)}-'
-        '${_printDigits(special, 1)}${_bitsDigits(12, 3)}-'
-        '${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}';
-  }
-
-  String _bitsDigits(int bitCount, int digitCount) =>
-      _printDigits(_generateBits(bitCount), digitCount);
-
-  int _generateBits(int bitCount) => _random.nextInt(1 << bitCount);
-
-  String _printDigits(int value, int count) =>
-      value.toRadixString(16).padLeft(count, '0');
 }
